141acad0d5df9f1467f985d96fa22b8036435f95
Aladin <alaminlikhon283@gmail.com>
2022-12-05 17:35:46 +0600

    New Database

--- bot.js
+++ bot.js
@@ -32,7 +32,13 @@ let appNewForm = [];
 let isSettingFormUp = false;
 
 //______________________________[SAMP Server MySQL Connection]________________________________
-
+const mysql = require("oxmysql");
+var db = mysql.createConnection({
+    host: process.env.SQL_HOST,
+    user: process.env.SQL_USER,
+    password: process.env.SQL_PASS,
+    database: process.env.SQL_DB,
+});
 
 //_______________________________[BOT Startup]_________________________________________________
 //@audit-ok Client Ready
