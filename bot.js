dcc4c41e866864546fbab2d2f5a74c3755505203
Aladin151 <100511706+Aladin151@users.noreply.github.com>
2022-12-05 17:33:57 +0600

    STATUS

--- bot.js
+++ bot.js
@@ -32,13 +32,6 @@ let appNewForm = [];
 let isSettingFormUp = false;
 
 //______________________________[SAMP Server MySQL Connection]________________________________
-const mysql = require("mysql");
-var db = mysql.createConnection({
-    host: process.env.SQL_HOST,
-    user: process.env.SQL_USER,
-    password: process.env.SQL_PASS,
-    database: process.env.SQL_DB,
-});
 
 
 //_______________________________[BOT Startup]_________________________________________________
